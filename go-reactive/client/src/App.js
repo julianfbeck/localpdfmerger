5cfe5b79aba7a3c62c10cbd93ba1b73ea13f032d
Julian Beck <ju-fa-beck@t-online.de>
2021-01-24 11:33:57 +0100

    added style

--- go-reactive/client/src/App.js
+++ go-reactive/client/src/App.js
@@ -1,12 +1,60 @@
-import React, { Component, useEffect } from 'react'
+import React, { Component, useEffect, useMemo } from 'react'
 import download from 'downloadjs'
 import { useDropzone } from 'react-dropzone'
 import './App.css'
 const BrowserFS = require('browserfs')
 
 
+const baseStyle = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: '#eeeeee',
+  borderStyle: 'dashed',
+  backgroundColor: '#fafafa',
+  color: '#bdbdbd',
+  outline: 'none',
+  transition: 'border .24s ease-in-out'
+};
+
+const activeStyle = {
+  borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+  borderColor: '#00e676'
+};
+
+const rejectStyle = {
+  borderColor: '#ff1744'
+};
+
+
+
 function App () {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone()
+  const {acceptedFiles,
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject
+  } = useDropzone({accept: 'image/*'});
+
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isDragActive ? activeStyle : {}),
+    ...(isDragAccept ? acceptStyle : {}),
+    ...(isDragReject ? rejectStyle : {})
+  }), [
+    isDragActive,
+    isDragReject,
+    isDragAccept
+  ]);
+  
   const files = acceptedFiles.map(file => (
     <li key={file.path}>
       {file.path} - {file.size} bytes
@@ -63,19 +111,17 @@ function App () {
 
   return (
     <div className='App'>
-      <div className='App'>
-        <section className='container'>
-          <div {...getRootProps({ className: 'dropzone' })}>
-            <input {...getInputProps()} />
-            <p>Drag 'n' drop some files here, or click to select files</p>
-          </div>
+      <div className="container">
+      <div {...getRootProps({style})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+    </div>
           <aside>
             <h4>Files</h4>
             <ul>{files}</ul>
           </aside>
-        </section>
       </div>
-    </div>
   )
 }
 
