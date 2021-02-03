14964511b249986615142949f7b697bf8691768b
Julian Beck <ju-fa-beck@t-online.de>
2021-02-03 08:16:32 +0100

    add components

---
+++ src/components/dropzone.js
@@ -0,0 +1,68 @@
+import React, { useMemo, useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+
+const baseStyle = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '100px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: '#eeeeee',
+  borderStyle: 'dashed',
+  backgroundColor: '#fafafa',
+  color: '#bdbdbd',
+  outline: 'none',
+  transition: 'border .24s ease-in-out',
+  cursor: 'pointer'
+}
+
+const activeStyle = {
+  borderColor: '#2196f3'
+}
+
+const acceptStyle = {
+  borderColor: '#00e676'
+}
+
+const rejectStyle = {
+  borderColor: '#ff1744'
+}
+
+const DropzoneField = ({ setFiles }) => {
+  const onDrop = useCallback(acceptedFiles => {
+    acceptedFiles.map(async file => {
+      file.validated = false
+    })
+    setFiles(prev => [...prev, ...acceptedFiles])
+  }, [])
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject
+  } = useDropzone({ onDrop, accept: 'application/pdf' })
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {})
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  )
+
+  return (
+    <div className='container'>
+      <div {...getRootProps({ style })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+    </div>
+  )
+}
+
+export default DropzoneField
