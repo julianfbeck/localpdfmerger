f538caa9ba9030d47d16ec104749f0bb575bb5a0
Julian Beck <ju-fa-beck@t-online.de>
2021-02-09 13:08:05 +0100

    start moving to next.js

--- localpdf-texh/pages/_app.js
+++ localpdf-texh/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <div className="custom">
+      <Component {...pageProps} />
+      </div>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
