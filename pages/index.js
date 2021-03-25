59842755b539f5b835a404217b012526b8a1aece
Julian Beck <ju-fa-beck@t-online.de>
2021-03-25 12:00:27 +0100

    Removed: meta tags

--- pages/index.js
+++ pages/index.js
@@ -23,30 +23,7 @@ export default function Home() {
           content="Merge, PDF, Combine PDF, Local PDF, PDF Tools, Webassembly, pdfcpu, optimize, locally, Extract, Images, Extract Images from PDF, Extract Content from PDF, Extract Information, Extract Meta"
         />
         <meta name="author" content="Julian Beck" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@Kickbeak" />
-        <meta
-          name="twitter:title"
-          content="PDF Tools, Local PDF Tools - Right in your Browser"
-        />
-        <meta
-          name="twitter:description"
-          content="Merge multiple PDFs into one. Your files won't leave your System, Local PDF uses your Browser edit PDfs!"
-        />
-        <meta property="twitter:creator" content="kickbeak" />
-        <meta
-          name="twitter:image"
-          content="https://github.com/jufabeck2202/go-wasm-pdfmerge/blob/master/logo.jpeg?raw=true"
-        />
-        <meta
-          property="og:title"
-          content="Local PDF Tools - Merge, Optimize, Extract PDFs in your Browser"
-        />
-        <meta property="og:description" content="Local PDF uses Webassembly to edit your PDFs inside your Browser. Your files won't leave your System, they will not be sent to another server Currently, we support Merging PDFs, optimizing PDFs, and extracting Information like Images from PDF FilesMerge multiple PDFs into one" />
-        <meta property="og:image" content="https://github.com/jufabeck2202/go-wasm-pdfmerge/blob/master/logo.jpeg?raw=true" />
-        <meta property="og:url" content="localpdf.tech" />
-        <meta property="og:site_name" content="LocalPDF" />
-        <meta property="og:type" content="website" />
+
       </Head>
       <LandingLayout>
         <Hero
