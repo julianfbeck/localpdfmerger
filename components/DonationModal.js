2c1a961a327673954ae969aee5da70dd9f06ce42
Julian Beck <ju-fa-beck@t-online.de>
2022-03-30 20:07:35 +0200

    adds Total amount donatedd to modal

--- components/DonationModal.js
+++ components/DonationModal.js
@@ -5,15 +5,28 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
-
   Button,
   VStack,
-  Text
+  Text,
+  Progress,
 } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import DonationButton from "./DonationButton";
 
 const DonationModal = ({ isOpen, onOpen, onClose }) => {
+  const [donations, setDonations] = useState(0);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/payment")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data); // this will be a string
+        setDonations(data.total);
+      });
+  }, []);
+
   return (
     <>
       <Modal isOpen={isOpen} onClose={onClose}>
@@ -22,19 +35,32 @@ const DonationModal = ({ isOpen, onOpen, onClose }) => {
           <ModalHeader>Your file has been downloaded </ModalHeader>
           <ModalBody>
             <Text fontWeight="bold" mb="1rem">
-            Support this site ❤️
+              Support this site ❤️ by donating!
             </Text>
+            <Text mb="2">
+              The domain currently costs $30/year. So far people have donated $
+              {donations}
+            </Text>
+            <Progress hasStripe value={(donations * 3, 33)} />
           </ModalBody>
-            <VStack>
-              <DonationButton ammount={"5.00"} itemID="price_1IUw15J2iOysJZvPPgJ1Cgk3"></DonationButton>
-              <DonationButton ammount={"10.00"} itemID="price_1IUxxiJ2iOysJZvPiUiePnWK"></DonationButton>
-              <DonationButton ammount={"20.00"} itemID="price_1IUxyVJ2iOysJZvPbW7oqAcz"></DonationButton>
-
-            </VStack>
+          <VStack>
+            <DonationButton
+              ammount={"5.00"}
+              itemID="price_1IUw15J2iOysJZvPPgJ1Cgk3"
+            ></DonationButton>
+            <DonationButton
+              ammount={"10.00"}
+              itemID="price_1IUxxiJ2iOysJZvPiUiePnWK"
+            ></DonationButton>
+            <DonationButton
+              ammount={"20.00"}
+              itemID="price_1IUxyVJ2iOysJZvPbW7oqAcz"
+            ></DonationButton>
+          </VStack>
 
           <ModalFooter>
-            <Button variant="outline"  mr={3} onClick={onClose}>
-            Don't support me
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Don't support me
             </Button>
           </ModalFooter>
         </ModalContent>
